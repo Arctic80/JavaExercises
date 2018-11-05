c24c1640389e8bd97c4ed703cb0b572cdd783dfe
Arctic80 <37451716+Arctic80@users.noreply.github.com>
2018-11-05 13:08:47 +0100

    Add files via upload

---
+++ JavaProjects/src/Main.java
@@ -0,0 +1,93 @@
+import java.util.*;
+
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        char[] nomArray = {'D', 'a', 'v', 'i', 'd'};
+
+        List<Character> nom = iteraNomRetornaArray(nomArray);
+        System.out.println(iteraNomList(nom));
+        System.out.println(loadMap(nom));
+
+        List<Character> apellido = new ArrayList<>(Arrays.asList('P', 'a', 'r', 'd', 'o'));
+        List<Character> fullName = new ArrayList<>();
+
+        fullName.addAll(nom);
+        fullName.add(' ');
+        fullName.addAll(apellido);
+        System.out.println(joinFullName(nom, apellido));
+
+    }
+
+    private static List<Character> iteraNomRetornaArray(char[] nombre)
+    {
+        List<Character> nomList = new ArrayList<>();
+
+        for (char c : nombre)
+        {
+            nomList.add(c);
+            System.out.print(c);
+        }
+
+        System.out.print("\n");
+
+        return nomList;
+    }
+
+    private static String iteraNomList(List<Character> nombre)
+    {
+        StringBuilder strBuilder = new StringBuilder();
+
+        List<Character> vocales = new ArrayList<>(Arrays.asList('A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u'));
+
+        for (Character c : nombre)
+        {
+            if (Character.isDigit(c))
+            {
+                strBuilder.append("Els noms de persones no contenen números, ERROR \n");
+                break;
+            }
+            else
+            {
+                if (Character.isAlphabetic(c))
+                {
+                    if (vocales.contains(c)) strBuilder.append("VOCAL \n");
+                    else strBuilder.append("CONSONANT \n");
+                }
+                else
+                {
+                    strBuilder.append("El nom conté un caràcter no vàlid, ERROR \n");
+                    break;
+                }
+            }
+        }
+
+        return strBuilder.toString();
+    }
+
+    private static HashMap<Character, Integer> loadMap(List<Character> nomArray)
+    {
+        HashMap<Character, Integer> letrasMap = new HashMap<>();
+
+        for (Character c: nomArray)
+        {
+            if (letrasMap.get(c) != null) letrasMap.put(c, letrasMap.get(c) + 1);
+            else letrasMap.put(c, 1);
+        }
+
+        return letrasMap;
+    }
+
+    private static ArrayList<Character> joinFullName(List<Character> nombre, List<Character> apellido)
+    {
+        ArrayList<Character> fullname = new ArrayList<>();
+        fullname.addAll(nombre);
+        fullname.add(' ');
+        fullname.addAll(apellido);
+
+        return fullname;
+    }
+}
